d8ee791a57a94169ead9d17a09ebde6d19a6a966
breadfan18 <breadfan18@gmail.com>
2022-01-11 20:27:15 -0500

    created a embedeed favoritesSchema to the user model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const favoritesSchema = new Schema({
+    state: String,
+    totalCases: Number,
+    totalDeaths: Number,
+    totalRecovered: Number
+},
+{
+    timestamps: true
+})
+
 const userSchema = new Schema({
     firstName: String,
     lastName: String,
     email: String,
-    favorites: Array
+    favorites: [favoritesSchema]
 }, 
 {
     timestamps: true
