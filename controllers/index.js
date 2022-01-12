690af33668266ec8b1c64bd0df8d6d3a196e6097
breadfan18 <breadfan18@gmail.com>
2022-01-11 20:56:36 -0500

    created a seed route

---
+++ controllers/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const indexRouter = express.Router();
+
+const User = require('../models/user');
+const CovidData = require('../models/covidData');
+
+indexRouter.get('/data/seed', async (req, res) => {
+    const data = [
+        {
+            state: 'CA',
+            totalCases: Math.floor(Math.random() * (5000000 - 1000000) + 1000000),
+            totalDeaths: Math.floor(Math.random() * (200000 - 700000) + 200000),
+            totalRecovered: Math.floor(Math.random() * (1000000 - 3500000) + 1000000)
+        },
+        {
+            state: 'PA',
+            totalCases: Math.floor(Math.random() * (5000000 - 1000000) + 1000000),
+            totalDeaths: Math.floor(Math.random() * (200000 - 700000) + 200000),
+            totalRecovered: Math.floor(Math.random() * (1000000 - 3500000) + 1000000)
+        },
+        {
+            state: 'OH',
+            totalCases: Math.floor(Math.random() * (5000000 - 1000000) + 1000000),
+            totalDeaths: Math.floor(Math.random() * (200000 - 700000) + 200000),
+            totalRecovered: Math.floor(Math.random() * (1000000 - 3500000) + 1000000)
+        },
+        {
+            state: 'NY',
+            totalCases: Math.floor(Math.random() * (5000000 - 1000000) + 1000000),
+            totalDeaths: Math.floor(Math.random() * (200000 - 700000) + 200000),
+            totalRecovered: Math.floor(Math.random() * (1000000 - 3500000) + 1000000)
+        },
+        {
+            state: 'OR',
+            totalCases: Math.floor(Math.random() * (5000000 - 1000000) + 1000000),
+            totalDeaths: Math.floor(Math.random() * (200000 - 700000) + 200000),
+            totalRecovered: Math.floor(Math.random() * (1000000 - 3500000) + 1000000)
+        },
+        {
+            state: 'MI',
+            totalCases: Math.floor(Math.random() * (5000000 - 1000000) + 1000000),
+            totalDeaths: Math.floor(Math.random() * (200000 - 700000) + 200000),
+            totalRecovered: Math.floor(Math.random() * (1000000 - 3500000) + 1000000)
+        },
+    ]
+    await CovidData.deleteMany({}) //deleted everything in the database
+    await CovidData.create(data); //created new data from the seed data above. 
+    res.redirect('/data')
+})
+
+indexRouter.get('/data', async (req, res) => {
+    await res.send('do you have TP??');
+})
+
+
+module.exports = indexRouter;
